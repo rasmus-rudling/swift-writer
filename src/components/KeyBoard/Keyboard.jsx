bc8a672af9b0b5c2fa432b7ec5ff574a92126eb2
Rasmus Rudling <rasmusrudling@gmail.com>
2021-08-19 19:02:28 +0200

    fix: key doesn't show hover effect when it should be active

--- src/components/KeyBoard/Keyboard.jsx
+++ src/components/KeyBoard/Keyboard.jsx
@@ -1,61 +1,60 @@
-import React from 'react';
-import classes from './keyboard.module.scss';
-import Key from './Key/Key';
+import React from "react";
+import classes from "./keyboard.module.scss";
+import Key from "./Key/Key";
 
 const Keyboard = ({
-    keys, 
-    keyEnter, 
-    keyLeave, 
-    clearIndicators, 
-    activeKey,
-    updateTime,
-    allKeysActive,
-    extraClass
+	keys,
+	keyEnter,
+	keyLeave,
+	clearIndicators,
+	activeKey,
+	updateTime,
+	allKeysActive,
+	extraClass,
 }) => {
-    let keyboardClasses = [classes.Keyboard];
+	let keyboardClasses = [classes.Keyboard];
 
-    if (extraClass !== undefined) {
-        keyboardClasses.push(extraClass);
-    }
+	if (extraClass !== undefined) {
+		keyboardClasses.push(extraClass);
+	}
 
+	const keyIsActive = (_key) => {
+		if (_key.char === "Space") {
+			return activeKey === " ";
+		} else {
+			if (activeKey !== undefined) {
+				return activeKey.toUpperCase() === _key.char;
+			} else {
+				return false;
+			}
+		}
+	};
 
-    const keyIsActive = (_key) => {
-        if (_key.char === 'Space') {
-            return activeKey === " ";
-        } else {
-            return activeKey === _key.char;
-        }
-    } 
-
-    return (
-        <div 
-            className={keyboardClasses.join(" ")}
-            onMouseEnter = {clearIndicators}
-        >
-            {
-                keys.map(keyRow => (
-                    <div className={classes.row}>
-                        {
-                            keyRow.map(key => (
-                                <Key 
-                                    char = {key.char}
-                                    shiftChar = {key.shiftChar} 
-                                    altChar = {key.altGrChar}
-                                    width = {key.width}
-                                    finger = {key.finger}
-                                    keyLeave = {keyLeave}
-                                    keyEnter = {keyEnter}
-                                    active = {keyIsActive(key)}
-                                    updateTime = {updateTime}
-                                    allKeysActive = {allKeysActive}
-                                />
-                            ))
-                        }
-                    </div>
-                ))
-            }
-        </div>
-    )
-}
+	return (
+		<div
+			className={keyboardClasses.join(" ")}
+			onMouseEnter={clearIndicators}
+		>
+			{keys.map((keyRow) => (
+				<div className={classes.row}>
+					{keyRow.map((key) => (
+						<Key
+							char={key.char}
+							shiftChar={key.shiftChar}
+							altChar={key.altGrChar}
+							width={key.width}
+							finger={key.finger}
+							keyLeave={keyLeave}
+							keyEnter={keyEnter}
+							active={keyIsActive(key)}
+							updateTime={updateTime}
+							allKeysActive={allKeysActive}
+						/>
+					))}
+				</div>
+			))}
+		</div>
+	);
+};
 
 export default Keyboard;
